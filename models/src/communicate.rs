546ee74ded0b8f5d46dfce7e23aae90b70abe7a9
Junhao Zhu <63294481+realzhujunhao@users.noreply.github.com>
2023-03-01 01:08:45 +0900

    Delete communicate.rs

--- models/src/communicate.rs
+++
@@ -1,9 +0,0 @@
-use std::io;
-use crate::{message::Message, user::User};
-
-pub trait Communicate {
-    fn recv_from(from: &User, msg: Message) -> io::Result<()>;
-
-    // fn send_file() -> Result<(), Box<dyn Error>>;
-    // fn recv_file() -> Result<&'static [u8], Box<dyn Error>>;
-}
